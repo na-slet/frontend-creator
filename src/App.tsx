a1b875b59655e1bbb467ffe3b607654bf366ab59
huscker <egostalker03@mail.ru>
2023-01-23 23:42:09 +0300

    Added event listing

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,19 @@
 import NavBar from './components/NavBar/NavBar.tsx'
+import ListEvents from './components/ListEvents/ListEvents.tsx'
+import { Grid, Container } from '@mui/material';
 function App() {
   return (
     <div className="App">
       <NavBar/>
+      <Container maxWidth="xl" sx={{marginTop: 5}}>
+        <Grid container spacing={2}>
+          <Grid item xs={3}>
+            <ListEvents/>
+          </Grid>
+          <Grid item xs={9}>
+          </Grid>
+        </Grid>
+      </Container>
     </div>
   );
 }
